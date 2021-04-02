4ba5b7afdbec2bf76730ac8fab2cc0b8c1d7a28d
Joseph Graham <joseph@xylon.me.uk>
2021-04-02 18:14:45 +0100

    show environment

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World! This environment is "' \
+        + os.environ['APP_ENV'] + '"</h1>'
