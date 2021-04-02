4f55c073ce6c876a7f2d1b52fe0fb5ecdbc2abd9
Joseph Graham <joseph@xylon.me.uk>
2021-04-02 19:18:31 +0100

    use jinja templates

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import request
+from flask import render_template
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -20,24 +21,14 @@ class Farm(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    response = '<h1>Hello World! This environment is "' \
-        + os.environ['APP_ENV'] + '"</h1>'
-
     if request.method == 'POST':
         new_animal = Farm(animal=request.form['animal'], diet=request.form['diet'])
         db.session.add(new_animal)
         db.session.commit()
 
-    response += '<p>The farm has these animals:</p><ul>'
-    for entry in Farm.query:
-        response += '<li>' + entry.animal + ' eats ' + entry.diet + '</li>'
-
-    response += '</ul>'
-    
-    response += '''<form action="/" method="post"><br>
-Animal: <input type="text" name="animal">
-eats: <input type="text" name="diet">
-<input type="submit" value="Submit">
-</form>'''
-    
+    response = render_template('index.html',
+                               environment=os.environ['APP_ENV'],
+                               animals=Farm.query
+    )
+        
     return response
