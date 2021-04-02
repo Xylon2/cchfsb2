fc706fd24110b14ea487a311bcbd37d1b7b35fbd
Joseph Graham <joseph@xylon.me.uk>
2021-04-02 18:58:47 +0100

    db functionality

--- hello/__init__.py
+++ hello/__init__.py
@@ -3,6 +3,7 @@ import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import request
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = re.sub('postgres://', 'postgresql://', os.environ['DATABASE_URL'])
 
@@ -14,7 +15,26 @@ class Farm(db.Model):
     animal = db.Column(db.String(128))
     diet = db.Column(db.String(128))
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello World! This environment is "' \
+    response = '<h1>Hello World! This environment is "' \
         + os.environ['APP_ENV'] + '"</h1>'
+
+    if request.method == 'POST':
+        new_animal = Farm(animal=request.form['animal'], diet=request.form['diet'])
+        db.session.add(new_animal)
+        db.session.commit()
+
+    response += '<p>The farm has these animals:</p><ul>'
+    for entry in Farm.query:
+        response += '<li>' + entry.animal + ' eats ' + entry.diet + '</li>'
+
+    response += '</ul>'
+    
+    response += '''<form action="/" method="post"><br>
+Animal: <input type="text" name="animal">
+eats: <input type="text" name="diet">
+<input type="submit" value="Submit">
+</form>'''
+    
+    return response
