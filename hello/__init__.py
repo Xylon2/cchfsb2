5318f4b1ca13113877cf208d0cb7cd602c04abeb
Joseph Graham <joseph@xylon.me.uk>
2021-04-02 18:38:27 +0100

    db model and Flask-migrate

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,6 +1,18 @@
 import os
+import re
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = re.sub('postgres://', 'postgresql://', os.environ['DATABASE_URL'])
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Farm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    animal = db.Column(db.String(128))
+    diet = db.Column(db.String(128))
 
 @app.route('/')
 def index():
